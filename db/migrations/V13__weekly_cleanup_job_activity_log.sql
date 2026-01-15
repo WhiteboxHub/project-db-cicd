104527d2af712dbb7269eec5ffad300ef9c32d8e
Jawahar <reddynjawahar@gmail.com>
2026-01-15 10:01:47 -0800

    added weekly cleanup for job_activity_log

---
+++ db/migrations/V13__weekly_cleanup_job_activity_log.sql
@@ -0,0 +1,20 @@
+-- Add index for fast cleanup
+
+
+CREATE INDEX IF NOT EXISTS idx_job_activity_log_activity_date
+ON job_activity_log (activity_date);
+
+-- Drop old cleanup event if any
+
+DROP EVENT IF EXISTS ev_cleanup_job_activity_log;
+
+
+-- Weekly cleanup event
+
+
+CREATE EVENT ev_cleanup_job_activity_log
+ON SCHEDULE EVERY 1 WEEK
+STARTS CURRENT_TIMESTAMP
+DO
+DELETE FROM job_activity_log
+WHERE activity_date < CURDATE() - INTERVAL 7 DAY;
