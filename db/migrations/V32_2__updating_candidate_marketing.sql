1fa279996a8235b184fc408d0ed06411d04cb1dd
shivapatel7 <shivapatel098k@gmail.com>
2026-01-29 23:38:50 +0530

    added email sender engine table

--- db/migrations/V32_2__updating_candidate_marketing.sql
+++ db/migrations/V32_2__updating_candidate_marketing.sql
@@ -1,7 +1,5 @@
 ALTER TABLE candidate_marketing
-ADD COLUMN mass_email TINYINT(1) NOT NULL DEFAULT 0
-COMMENT '1 = allowed for mass email campaigns, 0 = not allowed';
+ADD COLUMN mass_email TINYINT(1) NOT NULL DEFAULT 0;
 
 ALTER TABLE candidate_marketing
-ADD COLUMN candidate_intro LONGTEXT
-COMMENT 'Large candidate introduction content for outreach ema
+ADD COLUMN candidate_intro LONGTEXT;
