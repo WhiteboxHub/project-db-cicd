1ad4cbfdc6204bde8c8a98adaded3be19a1c46d0
Rohit <rohithyadav628169@gmail.com>
2026-02-02 07:22:51 -0800

    implemented position_id column in interviews

--- db/migrations/V37_2__position_column_in_interview_table.sql
+++ db/migrations/V37_2__position_column_in_interview_table.sql
@@ -3,14 +3,14 @@ ALTER TABLE candidate_interview
   ADD COLUMN position_id BIGINT NULL
   COMMENT 'FK to position.id';
 
--- 2. Add index for performance 
+-- 2. Add index (required for FK performance)
 CREATE INDEX idx_candidate_interview_position_id
   ON candidate_interview(position_id);
 
--- 3. Add foreign key constraint
+-- 3. Add foreign key constraint (FIX: backticks)
 ALTER TABLE candidate_interview
   ADD CONSTRAINT fk_candidate_interview_position
   FOREIGN KEY (position_id)
-  REFERENCES position(id)
+  REFERENCES `position`(`id`)
   ON DELETE SET NULL
   ON UPDATE CASCADE;
