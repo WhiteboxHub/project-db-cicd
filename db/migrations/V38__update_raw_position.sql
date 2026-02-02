1251a6799ab0aba73f05a8e90564e45fe07e31af
manisaisaduvala@gmail.com <Manisai21>
2026-02-02 10:26:08 -0800

    fix changes candid id

--- db/migrations/V38__update_raw_position.sql
+++
@@ -1,7 +0,0 @@
--- Migration to add candidate_id to raw_position table
-ALTER TABLE raw_position 
-ADD COLUMN candidate_id INT DEFAULT NULL COMMENT 'ID from candidate_marketing table - tracks which candidate inbox this came from'
-AFTER id;
-
--- Add index for performance when filtering by candidate
-CREATE INDEX idx_candidate_id ON raw_position(candidate_id);
