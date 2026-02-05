3555f8fb872a41ab30ae9d7a7638b38fb4628b51
shivapatel7 <shivapatel098k@gmail.com>
2026-02-06 02:10:53 +0530

    adding new row in job_definition and job_schedule

--- db/migrations/V47__add_missing_job_columns.sql
+++ db/migrations/V47__add_missing_job_columns.sql
@@ -1,9 +1,9 @@
--- 1. Add email_engine_id to job_definition
+-- Add email_engine_id to job_definition
 ALTER TABLE job_definition
-ADD COLUMN IF NOT EXISTS email_engine_id INT NULL
+ADD COLUMN email_engine_id INT NULL
 AFTER candidate_marketing_id;
 
--- 2. Add manually_triggered to job_schedule
+-- Add manually_triggered to job_schedule
 ALTER TABLE job_schedule
-ADD COLUMN IF NOT EXISTS manually_triggered BOOLEAN NOT NULL DEFAULT FALSE
+ADD COLUMN manually_triggered BOOLEAN NOT NULL DEFAULT FALSE
 AFTER enabled;
