33b11b7fc36034836e4c43ffcefbd641a8b62f10
Jawahar <reddynjawahar@gmail.com>
2026-02-26 17:19:28 -0800

    reordered parent table

--- db/migrations/V73__drop_unused_tables.sql
+++ db/migrations/V73__drop_unused_tables.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS job_definition;
-DROP TABLE IF EXISTS job_request;
 DROP TABLE IF EXISTS job_run;
+DROP TABLE IF EXISTS job_request;
+DROP TABLE IF EXISTS job_definition;
 DROP TABLE IF EXISTS email_sender_engine;
