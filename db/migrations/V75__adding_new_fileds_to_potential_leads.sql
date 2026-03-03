2b2cab8b31b80dddcaf25c003dadf6233295daa2
Adarsh Tejakalakanda <adarshteja80@gmail.com>
2026-03-03 11:27:43 -0800

    this is the PR for adding new columns named outreach_message,outreach_connect for the potential leads table db

--- db/migrations/V75__adding_new_fileds_to_potential_leads.sql
+++ db/migrations/V75__adding_new_fileds_to_potential_leads.sql
@@ -1,9 +1,7 @@
 ALTER TABLE potential_leads
-  ADD COLUMN IF NOT EXISTS outreach_connection_status 
+  ADD COLUMN outreach_connection_status 
     ENUM('not_sent','sent','accepted') 
     DEFAULT 'not_sent',
-  ADD COLUMN IF NOT EXISTS outreach_message_status 
+  ADD COLUMN outreach_message_status 
     ENUM('not_sent','sent','responded') 
     DEFAULT 'not_sent';
-
-
