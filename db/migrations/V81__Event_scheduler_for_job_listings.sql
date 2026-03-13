075e62c7fc1217f842cc888754b384a3e812f5bd
Jatin Thakur <t.jatinsingh27@gmail.com>
2026-03-13 11:09:26 -0700

    Added an event scheduler that deletes jobs from linkedin and hiring.cafe sources older than 7 days and other sources older than 30 days

--- db/migrations/V81__Event_scheduler_for_job_listings.sql
+++ db/migrations/V81__Event_scheduler_for_job_listings.sql
@@ -1,6 +1,18 @@
--- Automatically delete job listings older than 7 days
+
+/* Event to clean old job listings based on source */
+/* This event scheduler deletes jobs from linkedin and hiring.cafe sources older than 7 days and other sources older than 30 days */
 CREATE EVENT IF NOT EXISTS cleanup_old_job_listings
 ON SCHEDULE EVERY 1 DAY
 DO
 DELETE FROM job_listing
-WHERE created_at < NOW() - INTERVAL 7 DAY;
+WHERE
+(
+    source IN ('linkedin','hiring.cafe')
+    AND created_at < NOW() - INTERVAL 7 DAY
+)
+OR
+(
+    source NOT IN ('linkedin','hiring.cafe')
+    AND created_at < NOW() - INTERVAL 30 DAY
+);
+
