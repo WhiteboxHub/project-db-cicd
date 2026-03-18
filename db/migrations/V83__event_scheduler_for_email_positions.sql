ab82422b8867e63f95b10442f4b1b3f82be6a43d
RohithYadav-21 <rohithyadav628169@gmail.com>
2026-03-18 08:36:16 -0700

    drop raw_job_listings & event schedular for email positions

---
+++ db/migrations/V83__event_scheduler_for_email_positions.sql
@@ -0,0 +1,12 @@
+
+
+-- Create cleanup event 
+CREATE EVENT IF NOT EXISTS `evt_cleanup_email_positions`
+  ON SCHEDULE EVERY 1 HOUR
+  STARTS CURRENT_TIMESTAMP
+  ON COMPLETION PRESERVE
+  ENABLE
+  COMMENT 'Deletes email_positions records older than 1 day'
+  DO
+    DELETE FROM `email_positions`
+    WHERE `created_at` < NOW() - INTERVAL 1 DAY;
