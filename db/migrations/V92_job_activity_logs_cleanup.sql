99cef19392591effec5bc1b1f3c3b86484cf1902
unknown <rudraprasada0107@gmail.com>
2026-04-15 09:05:15 -0700

    Jobs Logs deletion event Schedular updated

---
+++ db/migrations/V92_job_activity_logs_cleanup.sql
@@ -0,0 +1,11 @@
+DROP EVENT IF EXISTS ev_cleanup_job_activity_log;
+
+-- Recreate it to run daily and explicitly keep it enabled
+CREATE EVENT ev_cleanup_job_activity_log
+ON SCHEDULE EVERY 1 DAY
+STARTS CURRENT_TIMESTAMP
+ON COMPLETION PRESERVE
+ENABLE
+DO
+DELETE FROM job_activity_log
+WHERE lastmod_date_time < NOW() - INTERVAL 7 DAY;
