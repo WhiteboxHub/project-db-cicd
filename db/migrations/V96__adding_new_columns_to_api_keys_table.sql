6cc3df6cbcab960b6f69b97e8fb7c5cf08e24abb
AdarshTejakalakanda <140388015+AdarshTejakalakanda@users.noreply.github.com>
2026-04-24 14:32:09 -0700

    changing columns in candidate api key table --fixedv1

--- db/migrations/V96__adding_new_columns_to_api_keys_table.sql
+++ db/migrations/V96__adding_new_columns_to_api_keys_table.sql
@@ -1,3 +1,8 @@
-ALTER TABLE candidate_api_keys ADD COLUMN voice_enabled BOOLEAN DEFAULT FALSE;
-ALTER TABLE candidate_api_keys DROP COLUMN services_enabled;
-ALTER TABLE candidate_api_keys DROP INDEX unique_candidate_provider;
+ALTER TABLE candidate_api_keys 
+ADD COLUMN voice_enabled BOOLEAN DEFAULT FALSE;
+
+ALTER TABLE candidate_api_keys 
+DROP COLUMN services_enabled;
+
+ALTER TABLE candidate_api_keys 
+DROP INDEX unique_candidate_provider;
