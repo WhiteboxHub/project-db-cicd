1d7994591681fbae8cc5758056a085cfcc1fe5bd
AdarshTejakalakanda <140388015+AdarshTejakalakanda@users.noreply.github.com>
2026-04-24 14:43:41 -0700

    adding new table

--- db/migrations/V96__adding_new_columns_to_api_keys_table.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE candidate_api_keys 
-ADD COLUMN voice_enabled BOOLEAN DEFAULT FALSE;
-
-ALTER TABLE candidate_api_keys 
-DROP COLUMN services_enabled;
-
-ALTER TABLE candidate_api_keys 
-DROP INDEX unique_candidate_provider;
