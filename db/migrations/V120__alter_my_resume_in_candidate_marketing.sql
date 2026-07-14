9463b94e2a321d90adc779b4b9872c1ec1408024
Rudra-Prasad-30 <rudraprasada0107@gmail.com>
2026-07-14 13:59:48 -0700

    Altered marketing table changed the column name to  My_Resume and updated it to store PDF/doc/docx

---
+++ db/migrations/V120__alter_my_resume_in_candidate_marketing.sql
@@ -0,0 +1,6 @@
+ALTER TABLE candidate_marketing
+DROP COLUMN IF EXISTS my_resume;
+
+ALTER TABLE candidate_marketing
+ADD COLUMN My_Resume LONGBLOB NULL,
+ADD COLUMN my_resume_filename VARCHAR(255) NULL;
