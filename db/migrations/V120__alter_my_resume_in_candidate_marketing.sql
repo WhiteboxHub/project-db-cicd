0024ca5f15c7c0267882eb8342d3736df21f42d0
Rudra-Prasad-30 <rudraprasada0107@gmail.com>
2026-07-14 14:10:14 -0700

    Changed the name of the table to my_resume

--- db/migrations/V120__alter_my_resume_in_candidate_marketing.sql
+++ db/migrations/V120__alter_my_resume_in_candidate_marketing.sql
@@ -2,5 +2,5 @@ ALTER TABLE candidate_marketing
 DROP COLUMN my_resume;
 
 ALTER TABLE candidate_marketing
-ADD COLUMN My_Resume LONGBLOB NULL,
+ADD COLUMN my_resume LONGBLOB NULL,
 ADD COLUMN my_resume_filename VARCHAR(255) NULL;
