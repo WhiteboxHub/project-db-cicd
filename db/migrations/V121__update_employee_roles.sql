f65c9623b3b53ac1c6aadf4c94ba9efb16fdc1c1
Asim <khajaasimuddin73@gmail.com>
2026-07-20 12:58:42 -0700

    Add V121 migration to backfill employee and admin roles

---
+++ db/migrations/V121__update_employee_roles.sql
@@ -0,0 +1,11 @@
+-- Backfill existing employees so they don't lose their access
+UPDATE authuser a
+JOIN employee e ON a.uname = e.email
+SET a.role = 'employee'
+WHERE a.role IS NULL OR a.role = '';
+
+-- Set up the Admin role for multiple users
+UPDATE authuser 
+SET role = 'admin' 
+WHERE uname IN ('sampath.velupula@gmail.com', 'pathanrazakr745@gmail.com'); 
+
