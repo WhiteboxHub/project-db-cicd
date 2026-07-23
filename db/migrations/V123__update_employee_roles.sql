ed685ad15f93f29b206de0e41d30220940188388
khajaasimuddin73-dotcom <khajaasimuddin73@gmail.com>
2026-07-23 10:58:12 -0700

    Revert "user role set"

--- db/migrations/V123__update_employee_roles.sql
+++ db/migrations/V123__update_employee_roles.sql
@@ -4,9 +4,8 @@ JOIN employee e ON a.uname = e.email
 SET a.role = 'employee'
 WHERE a.role IS NULL OR a.role = '';
 
--- Set up the Admin role
+-- Set up the Admin role for multiple users
 UPDATE authuser 
 SET role = 'admin' 
-WHERE uname IN ('sampath.velupula@gmail.com', 'pathanrazakr745@gmail.com');
-
+WHERE uname IN ('sampath.velupula@gmail.com', 'pathanrazakr745@gmail.com'); 
 
